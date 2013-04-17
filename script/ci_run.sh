3413a9e682eedff900fda02e8e54838ae3a232e5
John Terry <jpterry@gmail.com>
2013-04-17 01:16:43 -0700

    More gem friendly build script

--- script/ci_run.sh
+++ script/ci_run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -e
 export PATH=./bin:$PATH
-bundle install --deployment --quiet --binstubs
+bundle install --quiet --binstubs --path=vendor/bundle
 SPEC_OPTS="--format html" bundle exec rake ci_test
